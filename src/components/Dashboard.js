ede49f1a2291e377d3a976af242047425126a8b0
John <nguyen8585@gmail.com>
2021-06-07 16:42:45 -0700

    first push

---
+++ src/components/Dashboard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import CreateProjectButton from './project/CreateProjectButton'
+import ProjectItem from './project/ProjectItem'
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <CreateProjectButton />
+                            <br />
+                            <hr />
+                            <ProjectItem />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
